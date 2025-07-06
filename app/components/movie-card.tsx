0d8711376d680d9e64faf0d38b11b4869e3ac4b0
Alireza Sarabchi <a.sarabchi@gmail.com>
2025-07-06 20:14:28 +0330

    Update MovieCard component for improved styling and structure; adjust global CSS variable for border radius; change default theme in Providers component to dark.

--- app/components/movie-card.tsx
+++ app/components/movie-card.tsx
@@ -2,6 +2,7 @@
 
 import { updateMovieData } from "@/actions/update-movie-data";
 import { Movie } from "@/lib/db/schema";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -34,61 +35,25 @@ export function MovieCard({ movie }: MovieCardProps) {
   };
 
   return (
-    <div className="">
-      <div className="flex flex-col gap-2">
-        <Image
-          src={movie.tvdbData?.image || "/placeholder.svg"}
-          alt={movie.title}
-          width={200}
-          height={600}
-          className="aspect-2/3 object-cover rounded-lg"
-        />
-
-        <div className="flex items-start justify-between">
-          <h3 className="font-medium text-neutral-900 dark:text-neutral-100">
-            {movie.title}
-            {movie.releaseYear && (
-              <span className="ml-1 text-neutral-500">
-                ({movie.releaseYear})
-              </span>
-            )}
-          </h3>
-        </div>
-        <p className="text-sm text-neutral-500 dark:text-neutral-400">
-          Watched on {formattedDate}
-        </p>
-        <div className="flex gap-2">
-          {movie.letterboxdUrl && (
-            <Link
-              href={movie.letterboxdUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="mt-2 inline-flex items-center text-sm text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300"
-            >
-              View on Letterboxd
-              <svg
-                className="ml-1 h-4 w-4"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-                />
-              </svg>
-            </Link>
-          )}
-          <button
-            onClick={handleGetTVDBData}
-            className="mt-2 text-sm text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300"
-          >
-            Get TVDB Data
-          </button>
-        </div>
+    <Link
+      href={`/movie/${movie.id}`}
+      className={cn(
+        ["relative overflow-hidden w-[200px] aspect-2/3 rounded-lg shadow-2xl"],
+        [
+          "after:absolute after:z-10 after:inset-0 after:border after:mix-blend-color-dodge after:w-[calc(100%-1px)] after:h-[calc(100%-1px)] after:rounded-[calc(var(--radius-lg)-1px)] after:border-white/10",
+        ]
+      )}
+    >
+      <Image
+        src={movie.tvdbData?.image || "/placeholder.svg"}
+        alt={movie.title}
+        fill
+        className="object-cover z-0"
+      />
+      <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-background via-background/60 to-background/0 p-2 pt-6 text-sm z-10">
+        <h2 className="font-semibold truncate">{movie.title}</h2>
+        <span className="opacity-80 text-xs">({movie.releaseYear})</span>
       </div>
-    </div>
+    </Link>
   );
 }
